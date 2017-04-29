73533eccba528a977f0c62c1960980074c7e5a79
Junaid Farooq <junaid@evercam.io>
2017-04-30 00:10:04 +0500

    Added Util.ex && method to grab changeset errors.

---
+++ lib/socialistical/util.ex
@@ -0,0 +1,9 @@
+defmodule Socialistical.Util do
+
+  def parse_changeset(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, fn
+      {msg, opts} -> String.replace(msg, "%{count}", to_string(opts[:count]))
+      msg -> msg
+    end)
+  end
+end
